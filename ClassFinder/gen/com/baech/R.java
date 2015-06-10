5a0ebfa4f880f88573879bd5a2a829c56a5dbab7
PureChaose <purechaose@gmail.com>
2015-06-10 10:48:35 -0400

    I did a thing

--- ClassFinder/gen/com/baech/R.java
+++ ClassFinder/gen/com/baech/R.java
@@ -8,6 +8,10 @@
 package com.baech;
 
 public final class R {
+    public static final class array {
+        public static final int spinner_grades_arr=0x7f040000;
+        public static final int spinner_preference_arr=0x7f040001;
+    }
     public static final class attr {
         /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
 or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
@@ -19,35 +23,65 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int metaButtonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int black_overlay=0x7f040000;
+        public static final int black_overlay=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dummy_button=0x7f080002;
-        public static final int fullscreen_content=0x7f080000;
-        public static final int fullscreen_content_controls=0x7f080001;
+        public static final int action_settings=0x7f0a0005;
+        public static final int continue_button=0x7f0a0004;
+        public static final int fullscreen_content=0x7f0a0000;
+        public static final int fullscreen_content_controls=0x7f0a0003;
+        public static final int spinner_grade=0x7f0a0001;
+        public static final int spinner_preference=0x7f0a0002;
     }
     public static final class layout {
         public static final int activity_profile_creator=0x7f030000;
+        public static final int activity_select_previous=0x7f030001;
+    }
+    public static final class menu {
+        public static final int select_previous=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int dummy_button=0x7f060003;
-        public static final int dummy_content=0x7f060004;
-        public static final int title_activity_profile_creator=0x7f060002;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int continue_button=0x7f070004;
+        /**  here are the descriptions for drop down menus 
+         */
+        public static final int dd_grade_title=0x7f07000a;
+        public static final int dd_preference_title=0x7f07000b;
+        public static final int dd_temp_title=0x7f07000c;
+        public static final int description=0x7f070006;
+        public static final int description_end=0x7f070007;
+        public static final int dummy_button=0x7f070003;
+        public static final int dummy_content=0x7f070005;
+        public static final int end_opt1=0x7f07000d;
+        public static final int end_opt2=0x7f07000e;
+        public static final int end_opt3=0x7f07000f;
+        public static final int end_opt4=0x7f070010;
+        public static final int end_opt5=0x7f070011;
+        public static final int end_opt6=0x7f070012;
+        public static final int end_opt7=0x7f070013;
+        public static final int end_opt8=0x7f070014;
+        public static final int hello_world=0x7f070009;
+        public static final int title_activity_profile_creator=0x7f070002;
+        public static final int title_activity_select_previous=0x7f070008;
     }
     public static final class style {
         /** 
@@ -71,15 +105,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int ButtonBar=0x7f070003;
-        public static final int ButtonBarButton=0x7f070004;
-        public static final int FullscreenActionBarStyle=0x7f070005;
-        public static final int FullscreenTheme=0x7f070002;
+        public static final int AppTheme=0x7f080001;
+        public static final int ButtonBar=0x7f080003;
+        public static final int ButtonBarButton=0x7f080004;
+        public static final int FullscreenActionBarStyle=0x7f080005;
+        public static final int FullscreenTheme=0x7f080002;
     }
     public static final class styleable {
         /** 
--- ClassFinder/src/com/baech/ProfileCreator.java
+++ ClassFinder/src/com/baech/ProfileCreator.java
@@ -60,44 +60,44 @@ public class ProfileCreator extends Activity {
 				HIDER_FLAGS);
 		mSystemUiHider.setup();
 		mSystemUiHider
-				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-					// Cached values.
-					int mControlsHeight;
-					int mShortAnimTime;
+		.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+			// Cached values.
+			int mControlsHeight;
+			int mShortAnimTime;
 
-					@Override
-					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-					public void onVisibilityChange(boolean visible) {
-						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-							// If the ViewPropertyAnimator API is available
-							// (Honeycomb MR2 and later), use it to animate the
-							// in-layout UI controls at the bottom of the
-							// screen.
-							if (mControlsHeight == 0) {
-								mControlsHeight = controlsView.getHeight();
-							}
-							if (mShortAnimTime == 0) {
-								mShortAnimTime = getResources().getInteger(
-										android.R.integer.config_shortAnimTime);
-							}
-							controlsView
-									.animate()
-									.translationY(visible ? 0 : mControlsHeight)
-									.setDuration(mShortAnimTime);
-						} else {
-							// If the ViewPropertyAnimator APIs aren't
-							// available, simply show or hide the in-layout UI
-							// controls.
-							controlsView.setVisibility(visible ? View.VISIBLE
-									: View.GONE);
-						}
-
-						if (visible && AUTO_HIDE) {
-							// Schedule a hide().
-							delayedHide(AUTO_HIDE_DELAY_MILLIS);
-						}
+			@Override
+			@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+			public void onVisibilityChange(boolean visible) {
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+					// If the ViewPropertyAnimator API is available
+					// (Honeycomb MR2 and later), use it to animate the
+					// in-layout UI controls at the bottom of the
+					// screen.
+					if (mControlsHeight == 0) {
+						mControlsHeight = controlsView.getHeight();
 					}
-				});
+					if (mShortAnimTime == 0) {
+						mShortAnimTime = getResources().getInteger(
+								android.R.integer.config_shortAnimTime);
+					}
+					controlsView
+					.animate()
+					.translationY(visible ? 0 : mControlsHeight)
+					.setDuration(mShortAnimTime);
+				} else {
+					// If the ViewPropertyAnimator APIs aren't
+					// available, simply show or hide the in-layout UI
+					// controls.
+					controlsView.setVisibility(visible ? View.VISIBLE
+							: View.GONE);
+				}
+
+				if (visible && AUTO_HIDE) {
+					// Schedule a hide().
+					delayedHide(AUTO_HIDE_DELAY_MILLIS);
+				}
+			}
+		});
 
 		// Set up the user interaction to manually show or hide the system UI.
 		contentView.setOnClickListener(new View.OnClickListener() {
@@ -114,7 +114,7 @@ public class ProfileCreator extends Activity {
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
 		// while interacting with the UI.
-		findViewById(R.id.dummy_button).setOnTouchListener(
+		findViewById(R.id.continue_button).setOnTouchListener(
 				mDelayHideTouchListener);
 	}
 
@@ -139,6 +139,7 @@ public class ProfileCreator extends Activity {
 			if (AUTO_HIDE) {
 				delayedHide(AUTO_HIDE_DELAY_MILLIS);
 			}
+			setContentView(R.layout.activity_select_previous);
 			return false;
 		}
 	};
